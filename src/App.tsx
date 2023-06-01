06a23667011d87019779af3dd7fa41f3a7c14313
Kevin Zheng <kaiwen530@gmail.com>
2023-06-01 22:53:16 +1000

    Implement card effect

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
   Avatar,
   Box,
   Button,
+  ButtonBase,
   Card,
   CardActions,
   CardContent,
@@ -32,6 +33,7 @@ import "./App.css";
 import { StackGrid, useStackGrid } from "./StackGrid";
 import { TagListEditor } from "./TagListEditor";
 import { ManagedModal, AppBarTitle as Title } from "./generic/Modal";
+import Tilt from "react-parallax-tilt";
 
 const images = [
   "https://images.unsplash.com/photo-1614597396930-cd6760b99f7c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1280&q=80",
@@ -55,63 +57,77 @@ function ImageCard({ image }: { image?: string }) {
 
   return (
     <Box sx={{ padding: 1 }}>
-      <Card
-        ref={ref}
-        sx={{
-          borderRadius: 6,
-          aspectRatio: (dims?.width ?? 1) / (dims?.height ?? 1),
-          boxShadow: "none",
-          position: "relative",
-        }}
+      <Tilt
+        glareEnable
+        glareMaxOpacity={0.4}
+        glareColor="#ffffff"
+        glarePosition="bottom"
+        glareBorderRadius="20px"
+        tiltMaxAngleX={4}
+        tiltMaxAngleY={4}
       >
-        <Parallax
-          renderLayer={(r) => (
-            <div
-              style={{
-                top: 0,
-                left: 0,
-                transform: `translateY(calc(${r * 100}px - 50%)) scale(2)`,
-                position: "absolute",
-                backgroundImage: `url(${image})`,
-                backgroundSize: "cover",
-                width: "100%",
-                height: "100%",
-              }}
-            />
-          )}
-          style={{ height: "100%" }}
-          strength={100}
-          blur={{ min: -10, max: 10 }}
-        ></Parallax>
-        <Fade in={isHover}>
-          <Box
-            sx={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              background: alpha("#000", 0.6),
-              overflow: "auto",
-            }}
-          >
-            <CardContent>
-              <Chip sx={{ p: 2.5, m: 0.5 }} clickable label="Landscape" />
-              <Chip sx={{ p: 2.5, m: 0.5 }} clickable label="Clouds" />
-              <Chip sx={{ p: 2.5, m: 0.5 }} clickable label="Sky" />
-              <Chip sx={{ p: 2.5, m: 0.5 }} clickable label="Mountain" />
-            </CardContent>
-            <CardActions
-              sx={{
-                justifyContent: "center",
-              }}
-            >
-              <Button size="small">Edit Tags</Button>
-              <Button size="small">Delete</Button>
-            </CardActions>
-          </Box>
-        </Fade>
-      </Card>
+        <Card
+          ref={ref}
+          sx={{
+            borderRadius: 6,
+            aspectRatio: (dims?.width ?? 1) / (dims?.height ?? 1),
+            boxShadow: "none",
+            position: "relative",
+          }}
+        >
+          <ButtonBase sx={{ width: "100%", height: "100%", display: "block" }}>
+            <Parallax
+              renderLayer={(r) => (
+                <div
+                  style={{
+                    top: 0,
+                    left: 0,
+                    filter: isHover ? `blur(10px)` : "",
+                    transition: "0.5s filter",
+                    transform: `translateY(calc(${r * 200}px - 50%)) scale(2)`,
+                    position: "absolute",
+                    backgroundImage: `url(${image})`,
+                    backgroundSize: "cover",
+                    width: "100%",
+                    height: "100%",
+                  }}
+                />
+              )}
+              style={{ height: "100%" }}
+              strength={100}
+              blur={{ min: -10, max: 10 }}
+            ></Parallax>
+            <Fade in={isHover}>
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                  background: alpha("#000", 0.6),
+                  overflow: "auto",
+                }}
+              >
+                <CardContent>
+                  <Chip sx={{ p: 2.5, m: 0.5 }} clickable label="Landscape" />
+                  <Chip sx={{ p: 2.5, m: 0.5 }} clickable label="Clouds" />
+                  <Chip sx={{ p: 2.5, m: 0.5 }} clickable label="Sky" />
+                  <Chip sx={{ p: 2.5, m: 0.5 }} clickable label="Mountain" />
+                </CardContent>
+                <CardActions
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Button size="small">Edit Tags</Button>
+                  <Button size="small">Delete</Button>
+                </CardActions>
+              </Box>
+            </Fade>
+          </ButtonBase>
+        </Card>
+      </Tilt>
     </Box>
   );
 }
