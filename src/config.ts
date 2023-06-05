846c175f6fe601538611ee7ca0cf2b906646f9a0
Kevin Zheng <kaiwen530@gmail.com>
2023-06-05 23:47:52 +1000

    Fix redirect url

--- src/config.ts
+++ src/config.ts
@@ -25,8 +25,8 @@ export const config = {
     // OPTIONAL - Hosted UI configuration
     oauth: {
       domain: "ass2.auth.ap-southeast-2.amazoncognito.com",
-      redirectSignIn: "http://localhost:5173/",
-      redirectSignOut: "http://localhost:5173/",
+      redirectSignIn: window.location.href,
+      redirectSignOut: window.location.href,
       responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
       //   scope: ["email"],
     },
